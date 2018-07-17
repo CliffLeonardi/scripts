d84ce2116ef3363a93236a8610a191e0e19604de
cliff <cliff.leonardi@simplisafe.com>
2018-07-17 18:39:15 -0400

    adds computer moves method and input while loop

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -3,4 +3,11 @@
 from random import randint
 
 user_input = input()
-valid_moves = ['rock', 'paper', 'scissors']
+valid_moves = ["rock", "paper", "scissors"]
+
+while user_input not in valid_moves:
+    print("invalid move, try again")
+    user_input = input()
+
+def computer_move():
+    return(valid_moves[randint(0,2)])
